9fb99def4666f58a84ef3fe25fb93a97001107fc
bedmiston1996 <bedmiston@brooklynfriends.org>
2015-07-13 15:33:24 +0000

    other changes

--- flixzy.rb
+++ flixzy.rb
@@ -3,6 +3,16 @@ recommended_
 def fav_artist
 end
 
+def artist =(artist)
+  @artist = artist
+end
+
+def artist
+  @artist
+end
+
+def song
+  
 def playlist
   artist = "#{name}"
   song = "#{song}"
